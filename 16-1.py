1f6d2c1a613391e86f4a87b5e49a8892527c8167
czw <598717026@qq.com>
2017-09-06 23:37:00 +0800

    Example for page 14-16

---
+++ 16-1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+df = pd.read_csv('iris.data', header=None)
+df.tail()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+Y = df.iloc[0:100, 4].values
+Y = np.where(Y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
+plt.xlabel('petal length')
+plt.ylabel('sepal length')
+plt.legend(loc='upper left')
+plt.show()
+
+'''
+import Perceptron
+ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, Y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of misclassifications')
+plt.show()
+'''
