efecdcb8d714d601546f74f3db14ba13278fef9b
xinyougui <598717026@qq.com>
2017-11-30 14:30:05 +0800

    add to page 161

--- unit6.py
+++ unit6.py
@@ -16,7 +16,7 @@ le = LabelEncoder()
 y = le.fit_transform(y)
 print(np.unique(y))
 
-#148
+#p148
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -24,7 +24,107 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 pipe_lr = Pipeline([('scl', StandardScaler()), ('pca', PCA(n_components=2)), ('clf', LogisticRegression(random_state=1))])
 pipe_lr.fit(X_train, y_train)
 print('Test Accuracy: %.3f' %pipe_lr.score(X_test, y_test))
+
+#p153
+
+from sklearn.cross_validation import StratifiedKFold
+
+kfold = StratifiedKFold(y = y_train, n_folds=10, random_state=1)
+scores = []
+for k, (train, test) in enumerate(kfold):
+    pipe_lr.fit(X_train[train], y_train[train])
+    score = pipe_lr.score(X_train[test], y_train[test])
+    scores.append(score)
+    print('Fold: %s, Class dist. : %s Acc: %.3f' % (k+1, np.bincount(y_train[train]), score))
+    
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+from sklearn.cross_validation import cross_val_score
+
+scroes = cross_val_score(estimator=pipe_lr, X=X_train, y=y_train, cv=10, n_jobs=1)
+print('CV accuracy scores: %s' %scores)
+print('CV accuracy %.3f +/- %.3f' %(np.mean(scores), np.std(scores)))
+
+#p155
+
+import matplotlib.pyplot as plt
+from sklearn.learning_curve import learning_curve
+
+pipe_lr = Pipeline([('scl', StandardScaler()), ('clf', LogisticRegression(penalty='l2', random_state=0))])
+train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr, X=X_train, y=y_train, train_sizes=np.linspace(0.1, 1.0, 10), cv=10, n_jobs=1)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(train_sizes, train_mean, color='blue', marker='o', markersize=5, label='training accuracy')
+plt.fill_between(train_sizes, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+plt.plot(train_sizes, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='validation accuracy')
+plt.fill_between(train_sizes, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+plt.grid()
+plt.xlabel('Number of training samples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.0])
+plt.show()
+
+#p156
+
+from sklearn.learning_curve import validation_curve
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(estimator=pipe_lr, X=X_train, y=y_train, param_name='clf__C', param_range=param_range, cv=10)
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std=np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean, color='blue', marker='o', markersize=5, label='training accuracy')
+plt.fill_between(param_range, train_mean + train_std, train_mean - train_std, alpha=0.15, color='blue')
+plt.plot(param_range, test_mean, color='green', linestyle='--', marker='s', markersize=5, label='validation accuracy')
+plt.fill_between(param_range, test_mean + test_std, test_mean - test_std, alpha=0.15, color='green')
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.show()
+
+print('========================================================')
+
+#p158
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+
+pipe_svc = Pipeline([('scl', StandardScaler()), ('clf', SVC(random_state=1))])
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100,0, 1000,0]
+param_grid = [{'clf__C':param_range, 'clf__kernel':['linear']}, {'clf__C':param_range, 'clf__gamma':param_range, 'clf__kernel':['rbf']}]
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
+
+#p159
+
+clf = gs.best_estimator_
+clf.fit(X_train, y_train)
+print('Test accuracy: %.3f' % clf.score(X_test, y_test))
+
+#p160
+
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=10, n_jobs=-1)
+scores = cross_val_score(gs, X, y, scoring='accuracy', cv=5)
+pirnt('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+#p161
+
+from sklearn.tree import DecisionTreeClassifier
+gs = GridSearchCV(estimator=DecisionTreeClassifier(random_state=0), param_grid=[{'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}], scoring='accuracy' cv=5)
+scores = cross_val_score(gs, X_train, y_train, Scoring='accuracy', cv=5)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
